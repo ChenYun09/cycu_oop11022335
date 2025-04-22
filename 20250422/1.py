7aa42b31eb60cde0152f3859b03072adea20169d
CHENYUN <s6511234@gmail.com>
2025-04-22 19:05:59 +0800

    Update 1.py

--- 20250422/1.py
+++ 20250422/1.py
@@ -1,14 +1,2 @@
-# 定義一個函式
-def greet(name):
-    return f"Hello, {name}!"
-
-# 定義一個變數
-PI = 3.14159
-
-# 定義一個類別
-class Calculator:
-    def add(self, a, b):
-        return a + b
-
-    def subtract(self, a, b):
-        return a - b
+def bus_info ():
+    print('hello')
