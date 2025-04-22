8b60d97219b5a786ad91b7cfe2759b6038bf2995
CHENYUN <s6511234@gmail.com>
2025-04-22 18:44:58 +0800

    Create 1.py

---
+++ 20250422/1.py
@@ -0,0 +1,14 @@
+# 定義一個函式
+def greet(name):
+    return f"Hello, {name}!"
+
+# 定義一個變數
+PI = 3.14159
+
+# 定義一個類別
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
